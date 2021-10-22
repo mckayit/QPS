95ba1aef9943062cdd2e94632850235b8cc7c34d
Lawrence mckay <Lawrence@mckayit.com>
2021-10-22 15:47:40 +1000

    update   22-10

--- get-mountpoints.ps1
+++ get-mountpoints.ps1
@@ -1,11 +1,91 @@
-$TotalGB = @{Name = "Capacity(GB)"; expression = { [math]::round(($_.Capacity / 1073741824), 2) } }
-$FreeGB = @{Name = "FreeSpace(GB)"; expression = { [math]::round(($_.FreeSpace / 1073741824), 2) } }
-$FreePerc = @{Name = "Free(%)"; expression = { [math]::round(((($_.FreeSpace / 1073741824) / ($_.Capacity / 1073741824)) * 100), 0) } }
-
-function get-mountpoints1
+function get-disksizeWithMountpoints
 {
-    $volumes = Get-WmiObject win32_volume -Filter "DriveType='3'"
-    $volumes | Select Name, Label, DriveLetter, FileSystem, $TotalGB, $FreeGB, $FreePerc | Format-Table -AutoSize
-}
+    <#
+ #... get All Moiuntpoints on a system
+.SYNOPSIS
+    get all mount points of disks on a system
+.DESCRIPTION
+    displays all disks sizings that are Fixed disk type. (3)  
+    this will also show the disks that have mountpoints and where thewy are mounted.. 
+    
+.PARAMETER one
+    Specifies Pram details.
+.PARAMETER two
+    Specifies Pram details
+.PARAMETER InputObject
+    Specifies the object to be processed.  You can also pipe the objects to this command.
+.EXAMPLE
+    C:\PS>
+    Example of how to use this cmdlet
+.INPUTS
+    Inputs to this cmdlet (if any)
+.OUTPUTS
+    Output from this cmdlet (if any)
+.NOTES
+    
+    Lawrence McKay
+    Lawrence@mckayit.com
+    McKayIT Solutions Pty 
+     
+    Date:    9 aug 2021
+      
+     ******* Update Version number below when a change is done.*******
+     
+    History
+    Version         Date                Name           Detail
+    ---------------------------------------------------------------------------------------
+    0.0.1           9 Aug 2021         Lawrence       Initial Coding
 
-get-mountpoints1
+#>
+     
+  
+
+    begin 
+    {
+        #Setting the info on how the sizing are done.
+                  
+    }
+    
+    process 
+    {
+
+        try
+        {
+            $volumes = Get-WmiObject win32_volume -Filter "DriveType='3'" | Sort-Object DriveLetter # -Descending
+
+
+            foreach ($volume in $volumes)
+            {
+                $TotalGB = [math]::round(($volume.Capacity / 1gb), 2) 
+            
+                $FreeGB = [math]::round(($volume.FreeSpace / 1Gb), 2) 
+        
+                $FreePerc = [math]::round(((($volume.FreeSpace / 1GB) / ($volume.Capacity / 1GB)) * 100), 0) 
+    
+                [PSCustomObject] @{
+                    Name            = $volume.name
+                    Label           = $volume.label
+                    DriveLetter     = $volume.driveletter
+                    FileSystem      = $volume.filesystem
+                    "Capacity(GB)"  = $TotalGB
+                    "FreeSpace(GB)" = $FreeGB
+                    "Free(%)"       = $FreePerc
+                }
+            }
+        }
+    
+  
+
+        catch 
+        {
+            Write-Host 'ERROR : $(_.Exception.Message)' -ForegroundColor Magenta
+        }
+
+    }
+
+    end 
+    {
+            
+    }
+
+}
