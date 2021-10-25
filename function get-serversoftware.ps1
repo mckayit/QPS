ccf72788bfdc96424ec6aee561ef752c9e19d18c
Lawrence mckay <Lawrence@mckayit.com>
2021-10-26 09:07:21 +1000

    get Software

--- function get-serversoftware.ps1
+++ function get-serversoftware.ps1
@@ -7,14 +7,16 @@ function get-serversoftware
 .DESCRIPTION
     gets the Software via Remote Registry from all the servers imputted in via the Array.
 
-    the array must have the following Fields "ServerName" as well as the "SID"
+    the array must have the following Fields "ServerName"
 .PARAMETER Servers
-    array must have the following Fields "ServerName" as well as the "SID"
+    array must have the following Fields "ServerName"
 
 .PARAMETER InputObject
     Specifies the object to be processed.  You can also pipe the objects to this command.
 .EXAMPLE
     C:\PS>Get-serverSoftware -Servers $arrayname
+    C:\PS>Get-serverSoftware -Servers SERVERNAME
+    
     C:\PS>Get-serverSoftware -Servers $arrayname |export-csv d:\serversoftware.csv 
     
 .INPUTS
@@ -35,6 +37,8 @@ function get-serversoftware
     Version         Date                Name           Detail
     ---------------------------------------------------------------------------------------
     0.0.1           19 May 2021         Lawrence       Initial Coding
+
+
 
 #>
      
@@ -62,55 +66,43 @@ function get-serversoftware
                 Activity         = "getting Server info"
                 Status           = "Processing [$i] of [$($Servers.Count)] users"
                 PercentComplete  = (($i / $Servers.Count) * 100)
-                CurrentOperation = "processing the following server : [ $($server.Servername)]"
+                CurrentOperation = "processing the following server : [ $($server)]"
             }
             Write-Progress @paramWriteProgress       
             $i++
+            $app = ""
+            $apps1 = @{}
 
 
+            ### Need to put in some error Checking to capture when it cant get onto Server Eg Blocked or can not find it.
 
-            New-pssession -ComputerName $server.serverName -ErrorAction  Silentlycontinue
-            $global:apps1 = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object Displayname, Publisher, Displayversion, VersionMajor, VersionMinor, Version, HelpLink, IrlInfoAbout, Comments, installDate 
-            $global:apps1 = $global:apps1 += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object Displayname, Publisher, Displayversion, VersionMajor, VersionMinor, Version, HelpLink, IrlInfoAbout, Comments, installDate
-                
+            #New-pssession -ComputerName $server -ErrorAction  Silentlycontinue
+            $apps1 = Invoke-command -computer $Server { Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object Displayname, Publisher, Displayversion, VersionMajor, VersionMinor, Version, HelpLink, IrlInfoAbout, Comments, installDate }
+            $apps1 = $apps1 += Invoke-command -computer $Server { Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object Displayname, Publisher, Displayversion, VersionMajor, VersionMinor, Version, HelpLink, IrlInfoAbout, Comments, installDate }
+   
+   
+          
 
-            Exit-PSSession
-            get-pssession | remove-pssession
-            $global:InstalledApps = $null
-            $global:InstalledApps = $apps1 | Select-Object | Where-Object { $_.Displayname.length -gt 2 }
-
-            <#      
-            [string]$Displayname = $global:InstalledApps.Displayname
-            [string]$Publisher = $global:InstalledApps.Publisher
-            [string]$Displayversion = $global:InstalledApps.Displayversion
-            [string]$VersionMajor = $global:InstalledApps.VersionMajor
-            [string]$VersionMinor = $global:InstalledApps.VersionMinor
-            [string]$Version = $global:InstalledApps.Version
-            [string]$HelpLink = $global:InstalledApps.HelpLink
-            [string]$IrlInfoAbout = $global:InstalledApps.IrlInfoAbout
-            [string]$Comments = $global:InstalledApps.Comments
-            [string]$installDate = $global:InstalledApps.installDate
-#>
-            <#
-               $global:outapps =[PSCustomObject]@{
-                    SID            = $Server.ServerSID 
-                    Displayname    = $Displayname
-                    Publisher      = $Publisher
-                    Displayversion = $Displayversion
-                    VersionMajor   = $VersionMajor
-                    VersionMinor   = $VersionMinor
-                    Version        = $Version
-                    HelpLink       = $HelpLink
-                    IrlInfoAbout   = $IrlInfoAbout
-                    Comments       = $Comments
-                    installDate    = $installDate
+            foreach ($app in $Apps1 | Select-Object | Where-Object { $_.Displayname.length -gt 2 })
+            {
+            
+               
+                #Outputting it as an array.          
+                [PSCustomObject]@{
+                    ServerName     = $Server
+                    Displayname    = $app.Displayname
+                    Publisher      = $app.Publisher
+                    Displayversion = $app.Displayversion
+                    #    VersionMajor   = $app.VersionMajor
+                    #    VersionMinor   = $app.VersionMinor
+                    #    Version        = $app.Version
+                    #    HelpLink       = $app.HelpLink
+                    #    IrlInfoAbout   = $app.IrlInfoAbout
+                    #    Comments       = $app.Comments
+                    #    installDate    = $app.installDate
                 }
-  #>          
+            }
   
-  
-  
-            $global:InstalledApps | add-member -type NoteProperty -name "SID" -value $($server.serversid)
-            $global:InstalledApps
         }
     }
 }
